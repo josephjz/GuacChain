2c97819065c7292087b207e3bc5085424a381865
Jennifer Joseph <josephjz@bc.edu>
2020-12-05 14:01:54 -0500

    GuacChain app started

--- GuacChain/ViewController.swift
+++ GuacChain/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tacoQtyLabel: UILabel!
+    @IBOutlet weak var burritoQtyLabel: UILabel!
+    @IBOutlet weak var chipsQtyLabel: UILabel!
+    @IBOutlet weak var horchataQtyLabel: UILabel!
+    @IBOutlet weak var bitcoinTotalLabel: UILabel!
+    @IBOutlet weak var currencyTotalLabel: UILabel!
+    
+    @IBOutlet weak var tacoStepper: UIStepper!
+    @IBOutlet weak var burritoStepper: UIStepper!
+    @IBOutlet weak var chipsStepper: UIStepper!
+    @IBOutlet weak var horchataStepper: UIStepper!
+    
+    @IBOutlet weak var currencySegmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func tacoStepperPressed(_ sender: UIStepper) {
+        tacoQtyLabel.text = "\(Int(sender.value))"
+    }
+    
+    @IBAction func burritoStepperPressed(_ sender: UIStepper) {
+        burritoQtyLabel.text = "\(Int(sender.value))"
+    }
+    
+    @IBAction func chipsStepperPressed(_ sender: UIStepper) {
+        chipsQtyLabel.text = "\(Int(sender.value))"
+    }
+    
+    @IBAction func horchataStepperPressed(_ sender: UIStepper) {
+        horchataQtyLabel.text = "\(Int(sender.value))"
+    }
+    
+    @IBAction func currencySegmentPressed(_ sender: UISegmentedControl) {
+    }
+    
+    
+    
 }
 
